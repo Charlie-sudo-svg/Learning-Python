aeb6ed3d7caafc32c37daa4bbf669f2d1a822034
Charlie <cppaley1@gmail.com>
2025-04-15 22:48:29 -0400

    Create Day2.py

---
+++ Day-2/Day2.py
@@ -0,0 +1,26 @@
+# Variables & Data Types
+
+age = 17
+print("I am" + str(age) + "years old")
+print("I am", age, "years old")
+print(f"I am {age} years old")
+
+#Integar 
+
+age = 100
+
+# Float 
+
+pi - 3.14159
+
+# String
+
+name = "Charlie"
+
+# Boolean
+
+ageis21 = False
+
+# Multiple Assignment
+
+a, b, c = 1, 2, 3
