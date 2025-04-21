f25cea84885162a561bcfad4412b746e150e4bd2
Charlie <cppaley1@gmail.com>
2025-04-21 17:10:49 -0400

    Create Day6.py

---
+++ Day6/Day6.py
@@ -0,0 +1,14 @@
+# Day 6 is all about comments 
+
+# A hastag (#) is used to explain code and is ignored by Python
+
+def greet(name):
+    """
+    This docstring is used to explain what a certain function or class does and
+
+    has
+
+    multiple
+
+    lines.
+    """
